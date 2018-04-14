e6af279bc1ad8c8baedf17aa34e11dacd3d0aeb4
ak096 <akshay913@gmail.com>
2018-04-14 22:32:41 +0200

    ros service scripts to be tested/finished

--- roboy_speech_recognition/scripts/stt_server.py
+++ roboy_speech_recognition/scripts/stt_server.py
@@ -8,6 +8,8 @@ import os
 import sys
 import signal
 import pyaudio
+import wave
+
 import traceback
 import pdb 
 
@@ -19,12 +21,16 @@ from roboy_communication_cognition.srv import RecognizeSpeech
 from roboy_communication_control.msg import ControlLeds
 from std_msgs.msg import Empty
 
+import speaker-recognition as sr
+
+WAV_PATH = "/home/akshay/hackroboy/src/roBauss/speaker-recognition/real_test/speaker.wav"
+MODEL_PATH = "/home/akshay/hackroboy/src/roBauss/speaker-recognition/src/model.out"
 
 
 BING_KEY = ''
 
 def stt_with_vad(bing):
-
+    print('stt_with_vad(bing)')
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
     RATE = 16000
@@ -40,12 +46,12 @@ def stt_with_vad(bing):
 
     pa = pyaudio.PyAudio()
     stream = pa.open(format=FORMAT,
-                               channels=CHANNELS,
-                               rate=RATE,
-                               input=True,
-                               start=False,
-                               # input_device_index=2,
-                               frames_per_buffer=CHUNK_SIZE)
+                     channels=CHANNELS,
+                     rate=RATE,
+                     input=True,
+                     start=False,
+                     #input_device_index=0,
+                     frames_per_buffer=CHUNK_SIZE)
 
 
     got_a_sentence = False
@@ -53,8 +59,9 @@ def stt_with_vad(bing):
 
 
     def handle_int(sig, chunk):
+        print('handle_int(sig, chunk)')
         global leave, got_a_sentence
-        
+
         leave = True
         got_a_sentence = True
         
@@ -98,11 +105,20 @@ def stt_with_vad(bing):
 
         sys.stdout.write('\n')
         data = b''.join(voiced_frames)
-        
+
+
+        wav_file = wave.open("/home/akshay/hackroboy/src/roBauss/speaker-recognition/real_test/speaker", 'wb')
+        wav_file.setnchannels(CHANNELS)
+        wav_file.setsampwidth(pyaudio.PyAudio().get_sample_size(FORMAT))
+        wav_file.setframerate(RATE)
+        wav_file.writeframes(data)
+        wav_file.close()
+
         stream.stop_stream()
         print("* done recording")
 
         # recognize speech using Microsoft Bing Voice Recognition
+        '''
         try:
             # pdb.set_trace()
             text = bing.recognize(data, language='en-US')
@@ -115,30 +131,36 @@ def stt_with_vad(bing):
             print("Microsoft Bing Voice Recognition could not understand audio")
         except RequestError as e:
             print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
-            
+        '''
+
+        text = "Hello," + sr.task_predict(WAV_PATH, MODEL_PATH)
         got_a_sentence = False
-            
+
+
+
     stream.close()
     return text
 
 def stt_subprocess(q):
-	q.put(stt_with_vad(bing))
+    print('stt_subprocess(q)')
+    q.put(stt_with_vad(bing))
 
 def handle_stt(req):
-	msg = ControlLeds()
-        msg.mode=2
-        msg.duration=0
-        ledmode_pub.publish(msg)
-        queue = Queue()
-	p = Process(target = stt_subprocess, args = (queue,))
-	p.start()
-	p.join()
-        msg = Empty()
-        ledfreeze_pub.publish(msg)
-	return queue.get()
+    print('handle_stt(req)')
+    msg = ControlLeds()
+    msg.mode=2
+    msg.duration=0
+    ledmode_pub.publish(msg)
+    queue = Queue()
+    p = Process(target = stt_subprocess, args = (queue,))
+    p.start()
+    p.join()
+    msg = Empty()
+    ledfreeze_pub.publish(msg)
+    return queue.get()
         #return stt_with_vad(bing)
-
 def stt_server():
+    print('stt_server()')
     #rospy.init_node('roboy_speech_recognition')
     s = rospy.Service('/roboy/cognition/speech/recognition', RecognizeSpeech, handle_stt)
     global ledmode_pub
@@ -148,11 +170,13 @@ def stt_server():
     global ledfreeze_pub
     ledfreeze_pub = rospy.Publisher("/roboy/control/matrix/leds/freeze", Empty, queue_size=1)
     rospy.init_node('roboy_speech_recognition')
-    global bing 
-    bing = BingVoice(BING_KEY)
-    
+    global bing
+    #bing = BingVoice(BING_KEY)
+    bing = 1
+    #print(stt_with_vad(0))
     print "Ready to recognise speech."
     rospy.spin()
 
 if __name__ == '__main__':
-	stt_server()
+    print('main')
+    stt_server()
